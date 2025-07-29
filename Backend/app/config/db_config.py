2c43c54af0eacd8e2350c29730a4f971ace86a14
Joseph Slater <josephslater03@gmail.com>
2025-07-29 15:55:17 -0400

    adding more db logic

--- Backend/app/config/db_config.py
+++ Backend/app/config/db_config.py
@@ -1,8 +1,13 @@
 from sqlmodel import SQLModel, create_engine, Session
+from app.models import comment_model, poll_model, poll_options_model, response_model, user_model
 
 DATABASE_URL = ""
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SQLModel.metadata.create_all(engine)
+
+# Remove echo true when deploying
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 def get_session():
     with Session(engine) as session:
